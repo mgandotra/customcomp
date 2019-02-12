657378209c054460861388a3774c25948036838a
mgandotra <mgandotra@sapient.com>
2019-02-12 14:14:49 +0530

    Update blocksdk.js

--- blocksdk.js
+++ blocksdk.js
@@ -3,7 +3,7 @@
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
- */
+ 
 
 var SDK = function (config, whitelistOverride, sslOverride) {
 	// config has been added as the primary parameter
@@ -138,7 +138,7 @@ SDK.prototype.triggerAuth = function (appID) {
 	});
 };
 
-/* Internal Methods */
+ Internal Methods 
 
 SDK.prototype._executePendingMessages = function _executePendingMessages () {
 	var self = this;
@@ -208,4 +208,31 @@ if (typeof(window) === 'object') {
 }
 if (typeof(module) === 'object') {
 	module.exports = SDK;
+}
+*/
+
+var BlockSDK = require('blocksdk');
+if (window.self === window.top) {
+	document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder Editor only.';
+} else {
+	var sdk = new BlockSDK();
+	sdk.getContent(function (content) {
+		var quill = new Quill('#editor-container', {
+			theme: 'snow'
+		});
+		quill.root.innerHTML = content;
+		function saveText() {
+			var html = quill.root.innerHTML;
+			sdk.setContent(html);
+			sdk.setSuperContent('This is super content: ' + html);
+
+			sdk.getData(function (data) {
+				var numberOfEdits = data.numberOfEdits || 0;
+				sdk.setData({
+					numberOfEdits: numberOfEdits + 1
+				});
+			});
+		}
+		quill.on('text-change', saveText);
+	});
 }
